8a722d9a2a40b392c7b02588586e754836ded8b6
Mathew <mateoicecua2000@gmail.com>
2024-08-26 20:06:22 -0500

    feat:  aside show detail of product

--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -2,18 +2,30 @@ import React from 'react'
 import { useState } from 'react'
 import { createContext } from 'react'
 
- export const ShoppingCartContext = createContext()
+export const ShoppingCartContext = createContext()
 
-export const ShoppingCartProvider = ({children}) => {
+export const ShoppingCartProvider = ({ children }) => {
 
-    const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0)
+  const [isProductDetailOpen, setIsProductDetailOpen] = useState(false)
+  const openProductDetail = () => setIsProductDetailOpen(true)
+  const closeProductDetail = () => setIsProductDetailOpen(false) 
+   
+  const [productToShow, setProductToShow] = useState({})
+
   return (
     <ShoppingCartContext.Provider value={{
-        count,
-        setCount
+      count,
+      setCount,
+      isProductDetailOpen,
+      setIsProductDetailOpen,
+      openProductDetail,
+      closeProductDetail,
+      productToShow, 
+      setProductToShow
     }}>
-     {children}
+      {children}
     </ShoppingCartContext.Provider>
-    
+
   )
 }
